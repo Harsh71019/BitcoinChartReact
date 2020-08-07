0a1fda6d78e09b5d026c5a8e10abc322c0400e3c
harsh <harsh710pat@gmail.com>
2020-08-07 19:04:21 +0530

    Updates

---
+++ precache-manifest.944b09ee92a7f6deeef863ca416169cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c13a6f72293a2bbd377cbd439245747",
+    "url": "/Harsh71019/BitcoinChartReact/index.html"
+  },
+  {
+    "revision": "dc29fbba302bb2a57657",
+    "url": "/Harsh71019/BitcoinChartReact/static/css/main.a7991542.chunk.css"
+  },
+  {
+    "revision": "002d77a863271757cecb",
+    "url": "/Harsh71019/BitcoinChartReact/static/js/2.cba32de1.chunk.js"
+  },
+  {
+    "revision": "ee4ee1ec0345baf3ccdd39c5f31204f6",
+    "url": "/Harsh71019/BitcoinChartReact/static/js/2.cba32de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc29fbba302bb2a57657",
+    "url": "/Harsh71019/BitcoinChartReact/static/js/main.b0e97dc7.chunk.js"
+  },
+  {
+    "revision": "5a618da4ceaa270172ab",
+    "url": "/Harsh71019/BitcoinChartReact/static/js/runtime-main.efc968fb.js"
+  }
+]);
